094ef5deeee6e88ff976ab675d175184ec9f9eac
Jeremy Seckinger <jtseck@aol.com>
2021-08-21 17:19:25 -0700

    Cleans up server.js

--- Server/server.js
+++ Server/server.js
@@ -28,6 +28,7 @@ server.use(express.urlencoded({ extended: false })); //middleware to get data fr
 server.use(express.json()); // accepts json data, added in case needed at some point
 server.use(cors());
 
+//* Logging  --> Only want morgan running in dev mode hence the 'if' statement
 if(process.env.NODE_ENV === 'development') {
     server.use(morgan('dev')) //morgan middleware
 }
@@ -39,79 +40,3 @@ server.use('/trips', tripsRoutes);
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-
-//! Previous server code below--refactored above
-
-// //* Logging  --> Only want morgan running in dev mode hence the 'if' statement
-// if(process.env.NODE_ENV === 'development') {
-//     server.use(morgan('dev')) //morgan middleware
-// }
-
-// //pasted from method-override docs and placed near top purposefully
-// //* Method override
-// server.use(
-//     methodOverride(function (req, res) {
-//         if (req.body && typeof req.body === 'object' && '_method' in req.body) {  // checks request body and looks for underscore method
-//              // look in urlencoded POST bodies and delete it
-//             let method = req.body._method // | replaces with whatever we add whether it is put or delete
-//             delete req.body._method       // |
-//             return method                 // |
-//         }
-//     })
-// )
-
-// //* Handlebars helpers
-// const { formatDate , stripTags, truncate, editIcon, select } = require('./helpers/hbs')  // destructured formatDate and bringing it in from helpers/hbs
-
-// //* Handlebars
-// server.engine(
-//     '.hbs', 
-//     exphbs({
-//         helpers: {  // Added helpers here within express handlebars
-//             formatDate, // bringing in date format helper
-//             stripTags,  //function from helpers/hbs
-//             truncate,   // ^^
-//             editIcon, //brings in edit icon function from hbs helper
-//             select, //helper brought in to select status of story in edit template 
-//         },
-//         defaultLayout: 'main', 
-//         extname: '.hbs',
-//     })
-// ); //adds middleware for handlebars
-// server.set('view engine', '.hbs');
-
-// //* Sessions (note: needs to be placed above passport middleware)(copy and pasted from express-session docs)
-// server.use(
-//     session({
-//         secret: 'keyboard cat',
-//         resave: false, // means nothing is saved if no modifications
-//         saveUninitialized: false, // set to false doesn't create a session until something is stored
-//         store: MongoStore.create({ 
-//             mongoUrl: process.env.MONGO_URI   // store added to create session storage in mongodb using .create taking in the mongo uri as an object
-//         }) 
-//     })
-// )
-
-
-// //* Passport middleware
-// server.use(passport.initialize()) // comes straight from passport docs
-// server.use(passport.session())
-
-// //* Set global variable
-// server.use(function(req, res, next) {  //passing in middleware function with access to request and response, with next being called when done
-//     res.locals.user = req.user || null //req.user is available with the auth middleware, and declaring it with res.locals.user creates a global variable accessible from within templates, and sets to null if it doesnt exist
-//     next()
-// })
-
-
-// //* Static folder
-// server.use(express.static(path.join(__dirname, 'public'))) // absolute path to current directory going into public
-
-// //* Routes
-// server.use('/', require('./routes/index'))
-// server.use('/auth', require('./routes/auth'))
-// server.use('/trips', require('./routes/trips'))
-
-// const PORT = process.env.PORT || 5000
-
-// server.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
