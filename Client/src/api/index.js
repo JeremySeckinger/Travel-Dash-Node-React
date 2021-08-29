147536076feaae6457fd991ad34d71a431aab22b
Jeremy Seckinger <jtseck@aol.com>
2021-08-29 08:21:41 -0700

    Implements likeTrip API call

--- Client/src/api/index.js
+++ Client/src/api/index.js
@@ -5,4 +5,5 @@ const url = 'http://localhost:5000/trips';
 export const fetchTrips = () => axios.get(url)
 export const createTrip = (newTrip) => axios.post(url, newTrip);//takes in entire post, then axios post specifying url and the data being sent which is the entire trip data
 export const updateTrip = (id, updatedTrip) => axios.patch( `${url}/${id}`, updatedTrip);
-export const deleteTrip = (id) => axios.delete(`${url}/${id}`)
+export const deleteTrip = (id) => axios.delete(`${url}/${id}`);
+export const likeTrip = (id) => axios.patch(`${url}/${id}/likeTrip`);
