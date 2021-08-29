ec3a3222c12accb5eec6794aa11566b475fedeb8
Jeremy Seckinger <jtseck@aol.com>
2021-08-28 19:26:44 -0700

    Implements onClick useDispatch deleteTrip functionality in components/trips/trip/trip.js

--- Client/src/components/Trips/Trip/Trip.js
+++ Client/src/components/Trips/Trip/Trip.js
@@ -3,8 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp, faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
 import { Card, Col, Button } from '@themesberg/react-bootstrap';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
+
+import { deleteTrip } from '../../../actions/trips'
 
 const Trip = ({ trip, setCurrentId }) => { //destructures trip and brings in to use for component below
+    const dispatch =  useDispatch(); 
     return (
 
             <Col className="">
@@ -19,7 +23,7 @@ const Trip = ({ trip, setCurrentId }) => { //destructures trip and brings in to 
                         <Button variant="secondary" size="sm" color="dark" className="w-100" onClick={() => {} }>view trip</Button>
                         <Card.Footer>
                             <Button variant="primary" size="sm" className="animate-up-2 mb-2 me-2" onClick={() => {} }><FontAwesomeIcon icon={faThumbsUp} /> Like {trip.likeCount}</Button>
-                            <Button variant="warning" size="sm" className="animate-down-2 mb-2 me-2" onClick={() => {} }><FontAwesomeIcon icon={faTrashAlt} /> Delete</Button>
+                            <Button variant="warning" size="sm" className="animate-down-2 mb-2 me-2" onClick={() => dispatch(deleteTrip(trip._id)) }><FontAwesomeIcon icon={faTrashAlt} /> Delete</Button>
                         </Card.Footer>
                     </Card.Body>
                 </Card>
