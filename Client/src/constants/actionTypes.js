e91a34c38bf94eaab987ee19d0a24867ea8e391b
Jeremy Seckinger <jtseck@aol.com>
2021-08-29 12:41:56 -0700

    Implements actionTypes in reducers as well

--- Client/src/constants/actionTypes.js
+++ Client/src/constants/actionTypes.js
@@ -1,5 +1,7 @@
 //* Implementing actionTypes is a safeguard to make sure errors are caught within actions-->misspelling actions results in no error handling or linting on its own
 //* for example misspelling the string 'CREATE' within actions will cause a failure that doesn't point to anything in particular-->as applications scale and more actions are created the mess gets bigger
+//! Used in reducers too
+
 
 export const CREATE = 'CREATE';
 export const UPDATE = 'UPDATE';
--- Client/src/reducers/trips.js
+++ Client/src/reducers/trips.js
@@ -1,13 +1,14 @@
+import { CREATE, UPDATE, DELETE, FETCH_ALL } from '../constants/actionTypes';
+
 export default (trips = [], action) => { //trips is the state here, and is equal to an empty array
     switch (action.type) { //switch used here instead of many if statements
-        case 'DELETE':
+        case DELETE:
             return trips.filter((trip) => trip._id !== action.payload); //filters out deleted trips
-        case 'UPDATE':
-        case 'LIKE': //Put below update using same logic since they are doing the same thing for different use cases
+        case UPDATE :
             return trips.map((trip) => trip._id === action.payload._id ? action.payload : trip); //mapping the trips array, updating the item in the array and returning the updated array
-        case 'FETCH_ALL': 
+        case FETCH_ALL: 
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             return [ ...trips, action.payload]; // returns trips using spread operator, and adds new trip that is stored in the action payload
         default:
             return trips;
