c5608fff5f55b2f25fed4de4419a313f5c03cc61
Jeremy Seckinger <jtseck@aol.com>
2021-12-27 16:10:16 -0800

    feat: pass setShowDefault prop from Trip through to Trips component

--- Client/src/components/Trips/Trips.js
+++ Client/src/components/Trips/Trips.js
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import Trip from './Trip/Trip';
 
 
-const Trips = ({ setCurrentId }) => {
+const Trips = ({ setCurrentId, setShowDefault }) => {
     const trips = useSelector((state) => state.trips);
 
     console.log(trips);
@@ -14,7 +14,7 @@ const Trips = ({ setCurrentId }) => {
         !trips.length ? <Spinner animation="border" role="status"><span className="visually-hidden">Loading...</span></Spinner> : (
         <Row xs={1} md={4} className="g-4">
             {trips.map((trip) => (
-                <Trip trip={trip} key={trip._id} setCurrentId={setCurrentId}/>
+                <Trip trip={trip} key={trip._id} setCurrentId={setCurrentId} setShowDefault={setShowDefault}/>
             ))}
         </Row>
         )
