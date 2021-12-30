22d82e7a37751f4c73d6279d2934a969760ad594
Jeremy Seckinger <jtseck@aol.com>
2021-12-29 16:28:46 -0800

    feat: update navbar with avatar component

--- Client/src/components/Form/Form.js
+++ Client/src/components/Form/Form.js
@@ -4,91 +4,128 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { createTrip, updateTrip } from "../../actions/trips";
 
-
-
 const PostTripForm = ({ currentId, setCurrentId, setShowDefault }) => {
-    const [tripData, setTripData] = useState({ title: '', status: '', body: '' }) 
-    const trip = useSelector((state) => currentId ? state.trips.find((t) => t._id === currentId) : null )
-    const dispatch = useDispatch();
-    const user = JSON.parse(localStorage.getItem('profile'));
+  const [tripData, setTripData] = useState({ title: "", status: "", body: "" });
+  const trip = useSelector((state) =>
+    currentId ? state.trips.find((t) => t._id === currentId) : null
+  );
+  const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
 
-    useEffect(() => {
-        if(trip) setTripData(trip);
-    }, [trip]) // <--dependency array (when the value changes from nothing-->trip then uses the above function)
+  console.log(user);
 
-    const handleSubmit = async (e) => { // once user submits--> send post request with all data entered
-        e.preventDefault(); // stops browser from refreshing
+  useEffect(() => {
+    if (trip) setTripData(trip);
+  }, [trip]); // <--dependency array (when the value changes from nothing-->trip then uses the above function)
 
-        if(currentId) {
-            dispatch(updateTrip(currentId, { ...tripData, name: user?.result?.name }));           
-        } else {
-            dispatch(createTrip({ ...tripData, name: user?.result?.name }));
-        }
+  const handleSubmit = async (e) => {
+    // once user submits--> send post request with all data entered
+    e.preventDefault(); // stops browser from refreshing
 
-        clear();
-    };
+    if (currentId) {
+      dispatch(
+        updateTrip(currentId, {
+          ...tripData,
+          name: user?.result?.name,
+          image: user?.result?.imageUrl,
+        })
+      );
+    } else {
+      dispatch(
+        createTrip({
+          ...tripData,
+          name: user?.result?.name,
+          image: user?.result?.imageUrl,
+        })
+      );
+    }
 
-    const clear = () => {
-        setCurrentId(null);
-        setTripData({ title: '', status: '', body:'' });
-        handleClose();
-    };
+    clear();
+  };
 
-    const handleClose = () => setShowDefault(false);
+  const clear = () => {
+    setCurrentId(null);
+    setTripData({ title: "", status: "", body: "" });
+    handleClose();
+  };
 
-    if(!user?.result?.name) {
-        return (
-            <div>
-                <span>
-                    Sign in to create your own trip
-                </span>
-            </div>
-        )
-    };
+  const handleClose = () => setShowDefault(false);
 
+  if (!user?.result?.name) {
     return (
-
-        <React.Fragment> 
-                <Card border="light" className="bg-white shadow-sm mb-4">
-                    <Card.Body>
-                    <h3>{ currentId ? 'Edit' : 'Add'} Trip</h3>
-                        <Row>
-                            <Form noValidate onSubmit={handleSubmit} className="col s12">
-                                <Row>
-                                    <Form.Group className="mb-3" id="title">
-                                        <Form.Label>Title</Form.Label>
-                                        {/* have to use the spread operator in this syntax to make the data persist below */}
-                                            <Form.Control type="text" placeholder="Enter trip title" name="title" value={tripData.title} onChange={(e) => setTripData({ ...tripData, title: e.target.value })}/> 
-                                    </Form.Group>
-                                </Row>
-                                <Row>
-                                    <Form.Group className="mb-3"> 
-                                        <Form.Label>Status</Form.Label> 
-                                        <Form.Select name="status" value={tripData.status} onChange={(e) => setTripData({ ...tripData, status: e.target.value })}>
-                                            <option>Select Public or Private</option>
-                                            <option value="public">Public</option>
-                                            <option value="private">Private</option>
-                                        </Form.Select>
-                                    </Form.Group>
-                                </Row>
-                                <Row>
-                                    <Form.Group className="mb-3">
-                                        <Form.Label sm="2">
-                                            Add trip details here
-                                        </Form.Label>
-                                        <Form.Control as="textarea" placeholder="Add trip details here" style={{ height: '100px' }} name="body" value={tripData.body} onChange={(e) => setTripData({ ...tripData, body: e.target.value })}/>
-                                    </Form.Group>
-                                </Row>
-                                <Col className="mt-3">
-                                    <Button variant="info" className="m-1" type="submit">Save</Button>
-                                    <Button variant="tertiary" className="m-1" onClick={clear}>Cancel</Button>
-                                </Col>
-                            </Form>
-                        </Row>
-                    </Card.Body>
-                </Card>
-        </React.Fragment>  
+      <div>
+        <span>Sign in to create your own trip</span>
+      </div>
     );
-}
+  }
+
+  return (
+    <React.Fragment>
+      <Card border="light" className="bg-white shadow-sm mb-4">
+        <Card.Body>
+          <h3>{currentId ? "Edit" : "Add"} Trip</h3>
+          <Row>
+            <Form noValidate onSubmit={handleSubmit} className="col s12">
+              <Row>
+                <Form.Group className="mb-3" id="title">
+                  <Form.Label>Title</Form.Label>
+                  {/* have to use the spread operator in this syntax to make the data persist below */}
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter trip title"
+                    name="title"
+                    value={tripData.title}
+                    onChange={(e) =>
+                      setTripData({ ...tripData, title: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Row>
+              <Row>
+                <Form.Group className="mb-3">
+                  <Form.Label>Status</Form.Label>
+                  <Form.Select
+                    name="status"
+                    value={tripData.status}
+                    onChange={(e) =>
+                      setTripData({ ...tripData, status: e.target.value })
+                    }
+                  >
+                    <option>Select Public or Private</option>
+                    <option value="public">Public</option>
+                    <option value="private">Private</option>
+                  </Form.Select>
+                </Form.Group>
+              </Row>
+              <Row>
+                <Form.Group className="mb-3">
+                  <Form.Label sm="2">Add trip details here</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    placeholder="Add trip details here"
+                    style={{ height: "100px" }}
+                    name="body"
+                    value={tripData.body}
+                    onChange={(e) =>
+                      setTripData({ ...tripData, body: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Row>
+              <Col className="mt-3">
+                <Button variant="info" className="m-1" type="submit">
+                  Save
+                </Button>
+                <Button variant="tertiary" className="m-1" onClick={clear}>
+                  Cancel
+                </Button>
+              </Col>
+            </Form>
+          </Row>
+        </Card.Body>
+      </Card>
+    </React.Fragment>
+  );
+};
 
 export default PostTripForm;
