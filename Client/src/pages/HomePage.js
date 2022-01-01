67e421b14cac81583056f15d7e685c70169e4d8c
Jeremy Seckinger <jtseck@aol.com>
2021-12-31 16:04:20 -0800

    chore: housekeeping

--- Client/src/pages/HomePage.js
+++ Client/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { Routes } from "../routes";
 
@@ -8,77 +8,99 @@ import Signin from "./Signin";
 import Signup from "./Signup";
 import ForgotPassword from "./examples/ForgotPassword";
 import ResetPassword from "./examples/ResetPassword";
-import Lock from "./examples/Lock";
 import NotFoundPage from "./examples/NotFound";
 import ServerError from "./examples/ServerError";
-import Users from "./dashboard/Users";
 import UserTrips from "./dashboard/UserTrips";
 // import AddTrip from "./dashboard/AddTrip";
 // import Login from "./Login";
-
 
 //* components
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import Preloader from "../components/Preloader";
 
-
 const RouteWithLoader = ({ component: Component, ...rest }) => {
-        const [loaded, setLoaded] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
-    useEffect(() => {
-        const timer = setTimeout(() => setLoaded(true), 1000);
-        return () => clearTimeout(timer);
-    }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => setLoaded(true), 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
-    return (
-        <Route {...rest} render={props => ( <> <Preloader show={loaded ? false : true} /> <Component {...props} /> </> ) } />
-    );
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        <>
+          {" "}
+          <Preloader show={loaded ? false : true} /> <Component {...props} />{" "}
+        </>
+      )}
+    />
+  );
 };
 
 const RouteWithSidebar = ({ component: Component, ...rest }) => {
-const [loaded, setLoaded] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
-    useEffect(() => {
-        const timer = setTimeout(() => setLoaded(true), 1000);
-        return () => clearTimeout(timer);
-    }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => setLoaded(true), 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
-
-    return (
-        <Route {...rest} render={props => (
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
         <>
-            <Preloader show={loaded ? false : true} />
-            <Sidebar />
+          <Preloader show={loaded ? false : true} />
+          <Sidebar />
 
-            <main className="content">
+          <main className="content">
             <Navbar />
             <Component {...props} />
-            </main>
+          </main>
         </>
-        )}
-        />
-    );
+      )}
+    />
+  );
 };
 
 export default () => (
-    <Switch>
-        <RouteWithLoader exact path={Routes.Signin.path} component={Signin} />
-        <RouteWithLoader exact path={Routes.Signup.path} component={Signup} />
-        <RouteWithLoader exact path={Routes.ForgotPassword.path} component={ForgotPassword} />
-        <RouteWithLoader exact path={Routes.ResetPassword.path} component={ResetPassword} />
-        <RouteWithLoader exact path={Routes.Lock.path} component={Lock} />
-        <RouteWithLoader exact path={Routes.NotFound.path} component={NotFoundPage} />
-        <RouteWithLoader exact path={Routes.ServerError.path} component={ServerError} />
+  <Switch>
+    <RouteWithLoader exact path={Routes.Signin.path} component={Signin} />
+    <RouteWithLoader exact path={Routes.Signup.path} component={Signup} />
+    <RouteWithLoader
+      exact
+      path={Routes.ForgotPassword.path}
+      component={ForgotPassword}
+    />
+    <RouteWithLoader
+      exact
+      path={Routes.ResetPassword.path}
+      component={ResetPassword}
+    />
+    <RouteWithLoader
+      exact
+      path={Routes.NotFound.path}
+      component={NotFoundPage}
+    />
+    <RouteWithLoader
+      exact
+      path={Routes.ServerError.path}
+      component={ServerError}
+    />
 
-        {/* pages */}
-        <RouteWithSidebar exact path={Routes.Trips.path} component={Trips} />
-        <RouteWithSidebar exact path={Routes.Users.path} component={Users} />
-        <RouteWithSidebar exact path={Routes.UserTrips.path} component={UserTrips} /> 
-        {/* <RouteWithSidebar exact path={Routes.AddTrip.path} component={AddTrip} />  */}
-        {/* <RouteWithSidebar exact path={Routes.Login.path} component={Login} />  */}
+    {/* pages */}
+    <RouteWithSidebar exact path={Routes.Trips.path} component={Trips} />
+    <RouteWithSidebar
+      exact
+      path={Routes.UserTrips.path}
+      component={UserTrips}
+    />
+    {/* <RouteWithSidebar exact path={Routes.AddTrip.path} component={AddTrip} />  */}
+    {/* <RouteWithSidebar exact path={Routes.Login.path} component={Login} />  */}
 
-
-        <Redirect to={Routes.NotFound.path} />
-    </Switch>
+    <Redirect to={Routes.NotFound.path} />
+  </Switch>
 );
