18581c7515da30ca2e5b1584494f948c80b3104f
Jeremy Seckinger <jtseck@aol.com>
2022-01-12 19:22:41 -0800

    update for deploy

--- Server/server.js
+++ Server/server.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose') // Added in after adding MongoStore session into session middleware (line 36)
-const dotenv = require('dotenv');
-const morgan = require('morgan'); //shows requests made directly in console (HTTP method and such)
-const connectDB = require('./config/db');
+const express = require("express");
+const mongoose = require("mongoose"); // Added in after adding MongoStore session into session middleware (line 36)
+const dotenv = require("dotenv");
+const morgan = require("morgan"); //shows requests made directly in console (HTTP method and such)
+const connectDB = require("./config/db");
 const tripsRoutes = require("./routes/trips");
-const userRoutes = require('./routes/users');
-const cors = require('cors');
+const userRoutes = require("./routes/users");
+const cors = require("cors");
 
 //* Load config
-dotenv.config({ path: './config/.env' })
+dotenv.config({ path: "./config/.env" });
 
-connectDB()
+connectDB();
 
 const server = express();
 
 //* Middleware found on stack overflow to solve preflight response error
-server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
 });
 
 //* Body parser
@@ -28,19 +31,21 @@ server.use(express.json()); // accepts json data, added in case needed at some p
 server.use(cors());
 
 //* Logging  --> Only want morgan running in dev mode hence the 'if' statement
-if(process.env.NODE_ENV === 'development') {
-    server.use(morgan('dev')) //morgan middleware
+if (process.env.NODE_ENV === "development") {
+  server.use(morgan("dev")); //morgan middleware
 }
 
+server.use("/trips", tripsRoutes);
+server.use("/user", userRoutes);
 
-server.use('/trips', tripsRoutes);
-server.use('/user', userRoutes);
-
-// API confirmation 
-server.get('/', (req, res) => {
-    res.send('TRAVEL DASH API')
-})
+// API confirmation
+server.get("/", (req, res) => {
+  res.send("TRAVEL DASH API");
+});
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+server.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
